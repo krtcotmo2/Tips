5ebbae5b3c7b1605916f1d14e12bc6961d83f4cd
krtcotmo2 <krtcotmo2@gmail.com>
2019-06-08 11:40:42 -0400

    even steven and total sum

--- client/src/pages/Even/Even.js
+++ client/src/pages/Even/Even.js
@@ -7,47 +7,92 @@ export default class Even extends PureComponent {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      horizontal: 10,
-      vertical: 50
+      numPeople: 3,
+      tipPercent: 15,
+      personAmount:0,
+      totalBill:0,
+      taxAmount:0,
+      includeTax:true
     }
   }
- 
+  handleTipChange = value => {
+    this.setState({
+      tipPercent: value,
+      personAmount: this.calculatePerPerson(),
+    })
+    
+  };
+  handlePeopleChange = value => {
+    this.setState({
+      numPeople: value,
+      personAmount: this.calculatePerPerson(),
+    })
+  };
+  updateBill = event =>{    
+    const { name, value } = event.target;
+    this.setState({ [name]: parseFloat(value) });
+
+    let _this = this;
+    let _cb = this.calculatePerPerson
+    setTimeout(
+      function(){ 
+        _this.setState({
+          personAmount: _cb(),
+        }); 
+    }, 5);
+
+
+  }
+  changeTax = event =>{
+    this.setState({
+      includeTax: (event.target.checked),
+    });
+    let _this = this;
+    let _cb = this.calculatePerPerson
+    setTimeout(
+      function(){ 
+        _this.setState({
+          personAmount: _cb(),
+        }); 
+    }, 5);
+  }
+
+  calculatePerPerson = () => {
+    console.log(this.state)
+    let tippableAmount = this.state.includeTax ? this.state.totalBill : this.state.totalBill - this.state.taxAmount;
+    let totalTip = tippableAmount * (this.state.tipPercent/100);
+    let personTip = totalTip / this.state.numPeople;    
+    return this.state.includeTax ? personTip + (this.state.totalBill/this.state.numPeople) : personTip + ((this.state.totalBill - this.state.taxAmount) / this.state.numPeople) +( this.state.taxAmount /  this.state.numPeople);
+  }
+
   render() {  
-    const { horizontal, vertical } = this.state
+    const { numPeople, tipPercent, personAmount, totalBill, includeTax, taxAmount } = this.state;
     const horizontalLabels = {
-      0: 'Low',
-      50: 'Medium',
-      100: 'High'
+      5: '5',
+      15: '15',
+      25: '25',
     }
   
-    const verticalLabels = {
-      10: 'Getting started',
-      50: 'Half way',
-      90: 'Almost done',
-      100: 'Complete!'
-    }  
     return (
       <section className="mainHolder container">
         <h3>Even Steven</h3>
         <span>Number in Party</span>
-        <Slider
-          min={2}
-          max={20}
-          value={horizontal}
-
-          
-         />
-        <span style={{width:'50%', display:'inline-block'}}>Include Taxes</span><Switch offLabel="No" onLabel="Yes"/>        
+        <div id="peopleslider">
+          <Slider name="people" min={2} max={12} value={numPeople} handleLabel={numPeople.toString()}  onChange={this.handlePeopleChange}/>
+        </div>             
         <span>Total Bill</span>
-        <TextInput type="number"/>
+        <TextInput type="number"  name="totalBill" value={totalBill} onChange={this.updateBill} />
         <span>Tax Amount</span>
-        <TextInput type="number"/>
+        <TextInput type="number" name="taxAmount" value={taxAmount} onChange={this.updateBill}  />
+        <span style={{width:'50%', display:'inline-block'}}>Include Taxes</span>
+        <Switch offLabel="No" onLabel="Yes" checked={includeTax} onChange={this.changeTax}/>  
         <span>Tip Percent</span>
-        <Range min="2" max="20" name="points" />
-      
+        <div id="tipslider">
+          <Slider name="tipPercent" min={0} max={30} value={tipPercent} handleLabel={tipPercent.toString()} onChange={this.handleTipChange}  labels={horizontalLabels} />
+        </div>
         <span>Amount Per Person</span>
-        <TextInput type="number" disabled />
-        <Link to="/"><button className="btn">Home</button></Link> 
+        <TextInput disabled value={personAmount.toFixed(2)} />
+        <Link to="/"><button className="btn btnHome">Home</button></Link> 
       </section>
     )
   }
--- client/src/pages/TotalSum/TotalSum.js
+++ client/src/pages/TotalSum/TotalSum.js
@@ -1,21 +1,85 @@
 import React, { PureComponent } from 'react'
-import { Range, TextInput } from 'react-materialize';
+import { Switch, TextInput } from 'react-materialize';
 import {Link} from "react-router-dom";
+import Slider from 'react-rangeslider'
 
 export default class TotalSum extends PureComponent {
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      tipPercent: 15,
+      totalWTip:0,
+      totalBill:0,
+      taxAmount:0,
+      includeTax:true
+    }
+  }
+  
+  handleTipChange = value => {
+    this.setState({
+      tipPercent: value,
+      totalWTip: this.calculatePerPerson(),
+    })
+    
+  };
+
+  updateBill = event =>{    
+    const { name, value } = event.target;
+    this.setState({ [name]: parseFloat(value) });
+
+    let _this = this;
+    let _cb = this.calculatePerPerson
+    setTimeout(
+      function(){ 
+        _this.setState({
+          totalWTip: _cb(),
+        }); 
+    }, 5);
+
+
+  }
+  changeTax = event =>{
+    this.setState({
+      includeTax: (event.target.checked),
+    });
+    let _this = this;
+    let _cb = this.calculatePerPerson
+    setTimeout(
+      function(){ 
+        _this.setState({
+          totalWTip: _cb(),
+        }); 
+    }, 5);    
+  }
+
+  calculatePerPerson = () => {
+    console.log(this.state)
+    let tippableAmount = this.state.includeTax ? this.state.totalBill : this.state.totalBill - this.state.taxAmount;
+    let totalTip = tippableAmount * (this.state.tipPercent/100);    
+    return totalTip + this.state.totalBill;
+  }
   render() {
+    const {tipPercent, totalWTip, totalBill, includeTax, taxAmount } = this.state;
+    const horizontalLabels = {
+      5: '5',
+      15: '15',
+      25: '25',
+    }
     return (
       <section className="mainHolder container">
         <h3>Total Sum</h3>
         <span>Total Bill</span>
-        <TextInput type="number"/>
+        <TextInput type="number"  name="totalBill" value={totalBill} onChange={this.updateBill} />
         <span>Tax Amount</span>
-        <TextInput type="number"/>
+        <TextInput type="number" name="taxAmount" value={taxAmount} onChange={this.updateBill}  />
+        <span style={{width:'50%', display:'inline-block'}}>Include Taxes</span>
+        <Switch offLabel="No" onLabel="Yes" checked={includeTax} onChange={this.changeTax}/>  
         <span>Tip Percent</span>
-        <Range min="2" max="20" name="points" />
-      
-        <span>Amount with Tip</span>
-        <TextInput type="number" disabled />
+        <div id="tipslider">
+          <Slider name="tipPercent" min={0} max={30} value={tipPercent} handleLabel={tipPercent.toString()} onChange={this.handleTipChange} labels={horizontalLabels}/>
+        </div>
+        <span>Amount Per Person</span>
+        <TextInput disabled value={totalWTip.toFixed(2)} />
         <Link to="/"><button className="btn">Home</button></Link>
       </section>
     )
